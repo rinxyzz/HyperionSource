2004db04890e342db8d6795d93882c2e2c5e7f15
m1lks <107134739+rinxyzz@users.noreply.github.com>
2023-05-23 00:12:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// script.js
-
 window.addEventListener('DOMContentLoaded', () => {
-    const jsonDataContainer = document.getElementById('json-data');
+    const jsonDataList = document.getElementById('json-data-list');
 
-    // Define the GET values
-    const param1 = 'param1';
-    const param2 = 'param2';
-
-    // Construct the URL with GET values
-    const url = `https://raw.githubusercontent.com/rinxyzz/HyperionSource/main/members.json?param1=${param1}&param2=${param2}`;
-
-    // Fetch the JSON file from GitHub with GET values
-    fetch(url)
+    // Fetch the JSON file from GitHub
+    fetch('https://raw.githubusercontent.com/example/your-file.json')
         .then(response => response.json())
         .then(data => {
-            // Process and display the JSON data
-            jsonDataContainer.textContent = JSON.stringify(data, null, 2);
+            // Extract the desired key values from the JSON objects
+            const key = 'name'; // Replace 'yourKey' with the actual key name
+            const values = data.map(obj => obj[key]);
+
+            // Create list items for each value
+            values.forEach(value => {
+                const listItem = document.createElement('li');
+                listItem.textContent = value;
+                jsonDataList.appendChild(listItem);
+            });
         })
         .catch(error => console.log(error));
 });
