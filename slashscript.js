d4f60444b7fb9f404e3975be5bf400715affbd96
m1lks <107134739+rinxyzz@users.noreply.github.com>
2023-05-23 00:43:01 +0700

    Update slashscript.js

--- slashscript.js
+++ slashscript.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Fetch the list of slash commands from the Discord API
     fetch('https://discord.com/api/v9/applications/1103427996532097054/commands', {
         headers: {
-            'Authorization': 'MTEwMzQyNzk5NjUzMjA5NzA1NA.GHUrm1.lZuWoMmLeTnprWbG3OkIR0862vrzgLepKx3ttY'
+            'Authorization': 'Bot MTEwMzQyNzk5NjUzMjA5NzA1NA.GHUrm1.lZuWoMmLeTnprWbG3OkIR0862vrzgLepKx3ttY'
         }
     })
     .then(response => response.json())
